c474125edbc889b5d81935e8ef6802debab4a5d8
Isurumad <isurumad95@gmail.com>
2019-08-20 11:15:32 +0530

    Edit test file

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import math
 
 image_main = cv2.imread('C:/Users/S.S.Aludeniya/Desktop/project/images/initnew.jpg')
 image_read = cv2.imread('C:/Users/S.S.Aludeniya/Desktop/project/images/read_crop.jpg')
-cap = cv2.VideoCapture('C:/Users/S.S.Aludeniya/Desktop/project/images/ori.3gp')
+cap = cv2.VideoCapture('ori.3gp')
 
 ##filter object by color
 def filterImage(image):
@@ -16,7 +16,7 @@ def filterImage(image):
     mask = cv2.inRange(img_hsv,lower_bound,upper_bound)
     res = cv2.bitwise_and(image,image,mask=mask)
         
-    return mask,res
+    return image,mask,res
 
 def getSkeleton(mask):
     size = np.size(mask)
@@ -48,7 +48,7 @@ def drawLines(image,color):
         x1,y1,x2,y2 = lines[0]
         cv2.line(image,(x1,y1),(x2,y2),color,3)
 
-    return edges
+    return image,edges
 
 def erodeImage(image,iterations):
     erode = cv2.erode(image,None,iterations=iterations)
@@ -78,16 +78,16 @@ def calAngleInit(image):
     
 def calAngleRead(image):
     b,g,r= cv2.split(image)
-    g = np.array(g)
+    b = np.array(b)
     z = 0
-    index=[0,220,0,300]
-    for x in range(len(r[220])):
-        if(r[220][x]== 255):
+    index=[0,160,0,260]
+    for x in range(len(r[160])):
+        if(r[160][x]== 255):
             index[0]=x
             break
 
-    for x in range(len(r[350])):
-        if(r[350][x]== 255):
+    for x in range(len(r[260])):
+        if(r[260][x]== 255):
             index[2]=x
             break
 
@@ -106,40 +106,47 @@ def calAngleRead(image):
             return 1.0
         
 
-def calReading(res_main,main_angle):
+def calReading(main_angle):
     ##take frame of video
 
     while(1):
         ret,frame = cap.read()
-        image,res_read=filterImage(frame)
-        edges_read = drawLines(res_read,[0,0,255])
+        image,mask,res_read=filterImage(frame)
+        edges_read,edges = drawLines(res_read,[0,0,255])
+        
+        res_read = erodeImage(res_read,2)
         res_read = dilateImage(res_read,1)
-        res_read = erodeImage(res_read,5)
-    ##result for display    
-        result = cv2.bitwise_or(res_main,res_read)
-
+    
         read_angle = calAngleRead(res_read)
-        
         angle = (180.0-(main_angle+read_angle))
-        scale_angle = (500.0/(180-(main_angle*2)))*angle
+        scale_angle = math.ceil((500.0/(180-(main_angle*2)))*(angle))
+      
         
-        if(scale_angle < 0):
-            sacle_angle = 0.0
-            print('Reading : ',scale_angle) 
-        elif(scale_angle >500):
-            sacle_angle = 0.0
-            print('Reading : ',scale_angle) 
+        if(scale_angle <= 0):
+            print('Reading : ',0.0) 
         else:
-            print('Reading : ',scale_angle)
+            print('Reading : ',(scale_angle))
             
-        cv2.imshow('Frame',result)
+        cv2.imshow('Image',image)
+        cv2.imshow('Frame',edges)
 
-        k=cv2.waitKey(5) & 0xFF
+        k=cv2.waitKey(1) & 0xFF
         if k == 27:
             break
     cv2.destroyAllWindows()
-    
-def main():
+
+def drawRefLine(image):
+    b,g,r = cv2.split(image)
+    g = np.array(g)
+    for x in range(len(g[160])):
+        g[160][x]=255
+
+    for x in range(len(g[260])):
+        g[260][x]=255
+
+    image = cv2.merge([b,g,r])
+    return image
+def test():
 ##    mask_main,res_main = filterImage(image_main)
 ##    edges_main = drawLines(res_main,[255,0,0])
 ##    res_main = dilateImage(res_main,1)
@@ -149,12 +156,20 @@ def main():
     while(1):
         res,frame = cap.read()
         mask,res = filterImage(frame)
-        cv2.imshow('frame',mask)
+        edges = drawLines(res,[255,0,0])
+        dilate =dilateImage(erodeImage(edges,2),1)
+        calAngleRead(drawRefLine(dilate))
+        cv2.imshow('frame',drawRefLine(dilate))
         k=cv2.waitKey(1) & 0xFF
         if k == 27:
             break
     cv2.destroyAllWindows()
+
    
+    
+def main():
+    calReading(55.02)
+    
 if __name__ == '__main__':
     main()
     
