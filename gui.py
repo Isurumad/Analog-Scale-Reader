6aaa54befea49d8d29af5fb25aae85ff07c5352d
Isurumad <48212143+Isurumad@users.noreply.github.com>
2019-08-21 06:10:00 +0530

    Delete gui.py

--- gui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import*
-
-def display(image):
-    window = Tk()
-    window.title('Digital Meter')
-    canvas = Canvas(window,width=500,height=500)
-    canvas,pack()
-    chras.create_image(0,0,anchor=NW,image=image)
